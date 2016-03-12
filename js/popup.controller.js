778124918f0c9230804f59e73d824ae3d367c47c
Terry Horowitz <terrychorowitz@gmail.com>
2016-03-12 15:32:58 -0500

    changed click options

--- js/popup.controller.js
+++ js/popup.controller.js
@@ -1,49 +1,44 @@
 
-myApp.controller("PageController", function ($scope, DOHFactory, $log) {
-  
-  $scope.details=false
+myApp.controller("PageController", function ($scope, DOHFactory) {
+  var resultPage = angular.element(document.querySelector('#results'));
   $scope.loading = true;
   
-    getDOHInfo = function (searchParams) {
-      DOHFactory.accessRecords(searchParams)
-      .then(function(restaurantMatches){
-        //filter for records where grade was given
-        if (!restaurantMatches.length) return notFound();
-        return restaurantMatches.filter(function(e){
-          return e.grade;
-        });
-      })
-      .then(function(restaurantMatches){
-        $scope.loading = false;
-        
-        $scope.gradeDisplayed = true;
-        $scope.record = restaurantMatches[0];
-        $scope.date = new Date(restaurantMatches[0].inspection_date)
-
-        var letterGrade = restaurantMatches[0].grade.toLowerCase();
-        var myEl = angular.element(document.querySelector('#results'));
-        myEl.append('<img id="result" src="/../images/' + letterGrade + '-grade.jpg">');
+  getDOHInfo = function (searchParams) {
+    DOHFactory.accessRecords(searchParams)
+    .then(function(restaurantMatches){
+      //filter for records where grade was given
+      if (!restaurantMatches.length) return notFound();
+      return restaurantMatches.filter(function(e){
+        return e.grade;
       });
-    }
-    
-    function notFound () {
+    })
+    .then(function(restaurantMatches){
       $scope.loading = false;
-      $scope.notFound = true;
-    }
-    
-    $scope.showHideDetails = function () {
-      $scope.details === true ? $scope.details=false : $scope.details = true;
-    }
-    
-    DOHFactory.getRestaurantDetails()
-    .then(function(info){
-      var queryStr = ['zipcode=' + info[0], 'dba=' + info[1], 'building=' + info[2], 'phone=' + info[3]]
-      $scope.findRestaurant = getDOHInfo(queryStr);
+
+      $scope.gradeDisplayed = true;
+      $scope.record = restaurantMatches[0];
+      $scope.date = new Date(restaurantMatches[0].inspection_date)
+
+      var letterGrade = restaurantMatches[0].grade.toLowerCase();
+      
+      resultPage.append('<img id="result" src="/../images/' + letterGrade + '-grade.jpg">');
     });
-  
-  var result = document.getElementById('results');
-  result.addEventListener('mouseover', function(event){
-    result.append('<div id="more">click for details</div>')
-  })
+  }
+
+  function notFound () {
+    $scope.loading = false;
+    $scope.notFound = true;
+  }
+
+  $scope.showDetails = function () {
+    $scope.details = true;
+  }
+
+  DOHFactory.getRestaurantDetails()
+  .then(function(info){
+    var queryStr = ['zipcode=' + info[0], 'dba=' + info[1], 'building=' + info[2], 'phone=' + info[3]]
+    $scope.findRestaurant = getDOHInfo(queryStr);
+  });
+
     
 });
