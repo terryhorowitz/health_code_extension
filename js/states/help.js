8516211f80c07f553f63efb1b1bffd67262d1339
Terry Horowitz <terrychorowitz@gmail.com>
2016-03-13 19:36:36 -0400

    edits to help page

--- js/states/help.js
+++ js/states/help.js
@@ -3,11 +3,10 @@ myApp.config(function($stateProvider) {
     .state('help', {
       url: "/help",
       template: '<div>' +
-                  '<img class="help" src="/../images/not-found.png">' +
+                  '<img class="help" src="/../images/not-found.png"><br/>' +
                   '<div id="answer">' +
                     '<div class="item">Possible Reasons for Error:</div><br/>' +
-                    '<div class="detail">-This chrome extension only works with YELP restaurant searches</div><br/>' +
-                    '<div class="detail">-You may not be looking at a restaurant</div><br/>' +
+                    '<div class="detail">-Are you sure you\'re looking at a restaurant?</div><br/>' +
                     '<div class="detail">-This restaurant has not yet recieved a grade</div><br/>' +
                     '<div class="detail">-Search criteria does not match Dept. Of Health records</div><br/>' +
                   '<div>' +
